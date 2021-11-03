53c7a19e08665d34192c97937c5027b776ea1ae3
Jonas Kristensen <jona910a@edu.easj.dk>
2021-11-03 09:48:37 +0100

    testapi1

--- TestApi/Managers/RecordManager.cs
+++ TestApi/Managers/RecordManager.cs
@@ -11,7 +11,7 @@ namespace TestApi.Managers
         public static List<Record> records = new List<Record>
         {
             new Record {Artist = "Michael Jackson", Title = "Thriller", Duration = 360, PublicationYear = 1982},
-            new Record {Artist = "Kim Larsen", Title = "Det Bedste Til Mig og Mine Venner", Duration = 300, PublicationYear = 1977}
+            new Record {Artist = "Kim Larsen", Title = "Det Bedste Til Mig og Mine Venner", Duration = 240, PublicationYear = 1977}
         };
 
         public List<Record> GetAllRecords(string keyword)
